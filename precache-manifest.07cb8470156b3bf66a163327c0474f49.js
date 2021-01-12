173f8f396f32d9b03b52e94d5d8698cb74e663b6
CPMSP <cpetersonmsp@gmail.com>
2021-01-11 23:50:33 -0600

    Updates

---
+++ precache-manifest.07cb8470156b3bf66a163327c0474f49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca391b967abadf4afb6972b037ec76c",
+    "url": "/reactToDo/index.html"
+  },
+  {
+    "revision": "5b1498823773957c348d",
+    "url": "/reactToDo/static/css/main.c5902003.chunk.css"
+  },
+  {
+    "revision": "0624a6624a3c8e159209",
+    "url": "/reactToDo/static/js/2.eba1ffa7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactToDo/static/js/2.eba1ffa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b1498823773957c348d",
+    "url": "/reactToDo/static/js/main.ed3d291b.chunk.js"
+  },
+  {
+    "revision": "eb620d7c1ff3c35fb048",
+    "url": "/reactToDo/static/js/runtime-main.597e373b.js"
+  }
+]);
